878617935a5138d269b956ea57d0bec8094709f7
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-06-07 13:58:53 -0700

    Improved the script
    Uses the native nano module more
    Now downloads all required sources

--- create_pooler_vm.ps1
+++ create_pooler_vm.ps1
@@ -1,17 +1,16 @@
 $ErrorActionPreference = 'Stop'
 $thisDir = $PSScriptRoot
 
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
 # http://int-resources.ops.puppetlabs.net/ISO/Windows/2016TP5Pre-Build-14300.1000.160324-1723.RS1/
-# http://int-resources.ops.puppetlabs.net/VMware/Win64%20Drivers/
-# ESX Drivers - https://packages.vmware.com/tools/esx/latest/windows/x64/index.html
-# BitVise - https://bvdl.s3-eu-west-1.amazonaws.com/BvSshServer-Inst.exe
+# Latest Windows VMWare Tools https://packages.vmware.com/tools/esx/latest/windows/x64/index.html
 # SetRights - https://github.com/cloudbase/SetUserAccountRights/releases/download/1.0/SetUserAccountRights.exe 
 
 $targetPath = "C:\dev\transfer\nano.vmdk" #"$thisDir\nano.vmdk"
-#$isoPath = "$thisDir\14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO"
-#$vmwareToolsISOPath = "$thisDir\VMware-tools-windows-9.0.17-3817773.iso"
 $AdministratorPassword = ConvertTo-SecureString -AsPlaintext -Force "Qu@lity!"
 $tmpDir = "$thisDir\temp"
+$tempExtraFiles = "$tmpDir\ExtraFiles"
 $sourcesDir = "$thisDir\Sources"
 
 # Cleanup
@@ -32,10 +31,30 @@ if (-not (Test-Path -Path $SetRightsEXE)) {
   $url64bit = 'https://github.com/cloudbase/SetUserAccountRights/releases/download/1.0/SetUserAccountRights.exe'
   (New-Object System.Net.WebClient).DownloadFile($url64bit, $SetRightsEXE) | Out-Null
 }
-# TODO Grab VMWare Tools
-$VMwareToolsISOPath = "$sourcesDir\VMware-tools-windows-9.0.17-3817773.iso"
-# TODO Grab Server 2016 ISO
+#  Grab VMWare Tools
+$VMwareToolsISOPath = "$sourcesDir\VMware-tools.iso"
+if (-not (Test-Path -Path $VMwareToolsISOPath)) {
+  Write-Host "Getting list of VMWare tools to download..."
+  $rootURL = 'https://packages.vmware.com/tools/esx/latest/windows/x64/'
+  $url64bit = ''
+  # Web Scrape the Vmware Tools download site...
+  $response = Invoke-WebRequest -Uri $rootURL -UseBasicParsing
+
+  $url64bit = $response.Links | ? { $_.href.ToLower().EndsWith('.iso') } | % {
+    Write-Output "$($rootURL)$($_.href)"
+  } | Select -First 1
+  if ($url64bit -eq $null) { throw "Unable to determing the latest VMWare Tools package to download"}
+
+  Write-Host "Downloading $url64bit"
+  (New-Object System.Net.WebClient).DownloadFile($url64bit, $VMwareToolsISOPath) | Out-Null
+}
+#   Grab Server 2016 ISO
 $Server2016ISO = "$sourcesDir\14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO"
+if (-not (Test-Path -Path $Server2016ISO)) {
+  Write-Host "Downloading Server 2016 TP5 ISO..."  
+  $url64bit = 'http://int-resources.ops.puppetlabs.net/ISO/Windows/2016TP5Pre-Build-14300.1000.160324-1723.RS1/14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO'
+  (New-Object System.Net.WebClient).DownloadFile($url64bit, $Server2016ISO) | Out-Null
+}
 #   Grab Cloudbase Offline
 $CloudBaseOfflineInitZip = "$sourcesDir\CloudBase.zip"
 if (-not (Test-Path -Path $CloudBaseOfflineInitZip)) {
@@ -69,8 +88,6 @@ finally {
   Dismount-DiskImage $VMwareToolsISOPath
 }
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-
 # Extract the Open-SSH package
 Write-Host "Extracting Win-OpenSSH..."  
 $tempOpenSSH = "$tmpDir\openssh"
@@ -81,23 +98,43 @@ Write-Host "Extracting Cloud Base for Nano..."
 $tempCloudBase = "$tmpDir\cloudbase"
 [System.IO.Compression.ZipFile]::ExtractToDirectory($CloudBaseOfflineInitZip, $tempCloudBase)
 
+# Create the extra files layout...
+Write-Host "Creating Extra Files layout..."  
+$ExtraFilesPaths = @{
+  "$tempOpenSSH\OpenSSH-Win64" = "OpenSSH-Win64"
+  "$sourcesDir\setuser\*" = "OpenSSH-Win64\"
+  "$thisDir\Install-OpenSSH.PS1" = "Install-OpenSSH.ps1"
+}
+#  "$thisDir\SetupComplete.CMD" = "Windows\Setup\Scripts\"
+New-Item -Path $tempExtraFiles -ItemType Directory | Out-Null
+$ExtraFilesPaths.GetEnumerator() | % {
+  $item = $_
+  Write-Host "Adding files from $($item.Key) ..."
+  $DestPath = "${tempExtraFiles}\$($item.Value)"
+  # If it ends with '\' then the directory must exist prior to file copy
+  if ($DestPath.EndsWith('\')) {
+    if (-not (Test-Path -Path $DestPath)) {
+      New-Item -Path $DestPath -Type Directory | Out-Null
+    }
+  }
+  Copy-Item -Recurse -Path "$($item.Key)" -Destination $DestPath -Confirm:$false -Force | Out-Null
+}
+
+# Read in SetupComplete.CMD
+$SetupComplete = ''
+if (Test-Path -Path "$($thisDir)\SetupComplete.CMD") {
+  $SetupComplete = Get-Content "$($thisDir)\SetupComplete.CMD"
+}
+
 # Time to build Nano
 Write-Host "Time to build Nano...."
 $IsoPath = $Server2016ISO
 $MaxSize = 1.5GB
-$arch = "amd64"
 $DiskLayout = "BIOS"
 $PackageList = @()
 $ServerEdition = 'Standard'
 $NanoServerDir = "$($thisDir)\NanoServer"
 $featuresToEnable = @('File-Services')
-$ExtraDriversPaths = @($VmwareDriversPath)
-$ExtraFilesPaths = @{
-  "$tempOpenSSH\OpenSSH-Win64" = "OpenSSH-Win64"
-  "$sourcesDir\setuser\*" = "OpenSSH-Win64\"
-  "$thisDir\Install-OpenSSH.PS1" = "Install-OpenSSH.ps1"
-  "$thisDir\SetupComplete.CMD" = "Windows\Setup\Scripts\"
-}
 
 if(Test-Path $TargetPath)
 {
@@ -132,8 +169,10 @@ try
   New-NanoServerImage -MediaPath "${isoMountDrive}:\" -BasePath $NanoServerDir `
   -MaxSize $MaxSize -AdministratorPassword $AdministratorPassword -TargetPath $vhdPath `
   -DeploymentType 'Guest' -OEMDrivers:$false `
-  -Compute:$false -Storage:$true -Clustering:$false `
-  -Containers:$false -Packages $PackageList -Edition $ServerEdition
+  -Compute:$false -Storage:$true -Clustering:$false -Containers:$false  `
+  -Packages $PackageList -Edition $ServerEdition `
+  -EnableRemoteManagementPort:$true -CopyFiles (Get-ChildItem -Path $tempExtraFiles | % { Write-Output $_.Fullname }) `
+  -DriversPath $VmwareDriversPath -SetupCompleteCommands $SetupComplete
 }
 finally
 {
@@ -162,27 +201,8 @@ try
     if($lastexitcode) { throw "dism /Enable-Feature failed for feature: $featureName"}
   }
 
-  foreach($driverPath in $ExtraDriversPaths)
-  {
-    Write-Host "Adding driver path $driverPath ..."
-    & $dismPath /Add-Driver /image:$mountDir /driver:$driverPath /Recurse
-    if($lastexitcode) { throw "dism /Add-Driver failed for path: $driverPath"}
-  }
+#read-host -Prompt "press enter"
 
-  $ExtraFilesPaths.GetEnumerator() | % {
-    $item = $_
-
-    Write-Host "Adding files from $($item.Key) ..."
-    $DestPath = "${mountDir}\$($item.Value)"
-
-    # If it ends with '\' then the directory must exist prior to file copy
-    if ($DestPath.EndsWith('\')) {
-      if (-not (Test-Path -Path $DestPath)) {
-        New-Item -Path $DestPath -Type Directory | Out-Null
-      }
-    }
-    Copy-Item -Recurse -Path "$($item.Key)" -Destination $DestPath -Confirm:$false -Force | Out-Null
-  }
 }
 finally
 {
